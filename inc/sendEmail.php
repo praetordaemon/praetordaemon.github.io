44b58aeef998495df75c17f2565219596112ab0f
praetordaemon <iandragonswordpyro@gmail.com>
2017-10-19 23:26:47 -0500

    Omega

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,19 +1,24 @@
-<?php
+﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'iandragonswordpyro@gmail.com';
+$siteOwnersEmail = 'user@website.com';
 
 
 if($_POST) {
 
-   $name = trim(stripslashes($_POST['contactName']));
+   $fname = trim(stripslashes($_POST['contactFname']));
+   $lname = trim(stripslashes($_POST['contactLname']));
    $email = trim(stripslashes($_POST['contactEmail']));
    $subject = trim(stripslashes($_POST['contactSubject']));
    $contact_message = trim(stripslashes($_POST['contactMessage']));
 
-   // Check Name
-	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
+   // Check First Name
+	if (strlen($fname) < 2) {
+		$error['fname'] = "Please enter your first name.";
+	}
+	// Check Last Name
+	if (strlen($lname) < 2) {
+		$error['lname'] = "Please enter your last name.";
 	}
 	// Check Email
 	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
@@ -26,6 +31,8 @@ if($_POST) {
    // Subject
 	if ($subject == '') { $subject = "Contact Form Submission"; }
 
+	// Set Name
+	$name = $fname . " " . $lname;
 
    // Set Message
    $message .= "Email from: " . $name . "<br />";
@@ -50,13 +57,14 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again, or contact me at iajobst323@oxfordcityschoolsonline.com."; }
+      else { echo "Something went wrong. Please try again."; }
 		
 	} # end if - no validation error
 
 	else {
 
-		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response = (isset($error['fname'])) ? $error['fname'] . "<br /> \n" : null;
+		$response .= (isset($error['lname'])) ? $error['lname'] . "<br /> \n" : null;
 		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
 		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
 		
